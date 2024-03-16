afb5d866856732b9b89550d2a0d4e88c5becc0d7
gmankab <gmankab@gmail.com>
2024-03-16 10:02:56 +0400

    [u] using typst for typ -> pdf conversion
    [+] added typsy dependency
    [f] fixed links collor
    [u] font changed from opensans to inter

--- convert.py
+++ convert.py
@@ -2,46 +2,87 @@
 
 from pathlib import Path
 import subprocess
+import shutil
 
 
-repo_path = Path(__file__).parent.resolve()
-md_path = repo_path / 'md'
-typ_path = repo_path / 'typ'
-pdf_path = repo_path / 'pdf'
-
-
-def convert(
+def md_to_typ(
     source_dir: Path,
     target_dir: Path,
-    args: list[str] = [],
 ):
+    additional_text = '''\
+#set text(font: "Inter")
+#show link: set text(blue)
+'''
+    if target_dir.exists():
+        shutil.rmtree(
+            path=target_dir,
+        )
     target_dir.mkdir(
         parents=True,
         exist_ok=True,
     )
     source_file: Path = Path()
     for source_file in source_dir.iterdir():
-        target_file = target_dir / f'{source_file.stem}.{target_dir.name}'
+        target_file = target_dir / f'{source_file.stem}.typ'
+        print(
+            f'converting {source_file} to {target_file}'
+        )
+        result = subprocess.run(
+            ['pandoc', source_file, '--to=typst', '--output=/dev/stdout', '--wrap=none'],
+            capture_output=True,
+            check=True,
+            text=True,
+        )
+        output = result.stdout
+        with target_file.open('w', encoding = 'utf-8') as file:
+            file.write(additional_text + '\n' + output)
+
+
+def typ_to_pdf(
+    source_dir: Path,
+    target_dir: Path,
+    fonts_dir: Path,
+):
+    if target_dir.exists():
+        shutil.rmtree(
+            path=target_dir,
+        )
+    target_dir.mkdir(
+        parents=True,
+        exist_ok=True,
+    )
+    source_file: Path = Path()
+    for source_file in source_dir.iterdir():
+        target_file = target_dir / f'{source_file.stem}.pdf'
         print(
             f'converting {source_file} to {target_file}'
         )
         subprocess.run(
-            ['pandoc', source_file, '-o', target_file] + args,
+            ['typst', 'compile', source_file, target_file, f'--font-path={fonts_dir}'],
             check=True,
         )
 
 
-def main():
-    convert(
+def main(
+    repo_path: Path | None = None,
+):
+    if not repo_path:
+        repo_path = Path(__file__).parent.resolve()
+    md_path = repo_path / 'md'
+    typ_path = repo_path / 'typ'
+    pdf_path = repo_path / 'pdf'
+    fonts_dir = repo_path / 'fonts'
+    md_to_typ(
         source_dir=md_path,
         target_dir=typ_path,
     )
-    convert(
+    typ_to_pdf(
         source_dir=typ_path,
         target_dir=pdf_path,
-        args=['--pdf-engine=xelatex', '-V', 'mainfont="OpenSans"']
+        fonts_dir=fonts_dir,
     )
     
 
-main()
+if __name__ == '__main__':
+    main()
 
